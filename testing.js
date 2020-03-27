502ef3d6b5a56ab974770ea61d14734b49b55f47
Francis Chua <hwymonkey@gmail.com>
2020-03-27 12:33:28 -0700

    circle test

---
+++ testing.js
@@ -0,0 +1,11 @@
+<script>
+
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#00FF00";
+  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.fill();
+
+
+</script>
