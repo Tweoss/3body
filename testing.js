b27899960816b4586ed8a405211a85fd48169ae4
Francis Chua <hwymonkey@gmail.com>
2020-03-27 12:51:26 -0700

    circle test3

--- testing.js
+++ testing.js
@@ -2,6 +2,10 @@
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
 
-  ctx.fillStyle = "#00FF00";
-  ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.arc(100, 75, 16, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.arc(200, 50, 25, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.arc(300, 100, 36, 0, 2 * Math.PI);
   ctx.fill();
