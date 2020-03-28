c322ef77b1da052ad0007b5e709d632fe161a956
Francis Chua <hwymonkey@gmail.com>
2020-03-28 11:16:31 -0700

    three body5.980

--- testing.js
+++ testing.js
@@ -19,6 +19,11 @@ alert(Math.pow(Math.pow(circle[0].x-circle[1].x,2)+Math.pow(circle[0].y-circle[1
 alert(-400*(circle[0].x-circle[1].x)/(Math.sqrt(Math.pow(Math.pow(circle[0].x-circle[1].x,2)+Math.pow(circle[0].y-circle[1].y,2),3))));//.0142668014
 alert(Math.sqrt(Math.pow(Math.pow(circle[0].x-circle[1].x,2)+Math.pow(circle[0].y-circle[1].y,2)+Math.pow(circle[0].z-circle[1].z,2),3)));//Nan
 alert(Math.sqrt(Math.pow(Math.pow(circle[0].x-circle[1].x,2),3)));//8000
+
+alert(Math.pow(circle[0].y-circle[1].y,2));//
+alert(Math.pow(circle[0].z-circle[1].z,2));//
+
+
 alert(Math.pow(Math.pow(circle[0].x-circle[1].x,2),3));//64000000
 alert(Math.pow(circle[0].x-circle[1].x,2));//400
 alert(circle[0].x-circle[1].x);//-20
